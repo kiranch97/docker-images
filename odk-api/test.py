1f957e236cf4240873e86e014d01fe4b335385d8
Stan Guldemond <stanguldemond@gmail.com>
2019-12-06 15:16:45 +0100

    push from Sven to old repo

--- odk-api/test.py
+++ odk-api/test.py
@@ -8,10 +8,10 @@ dbm = DatabaseManager("postgresql://odk:pgodk@116.203.210.203:5432/odk")
 # result_laf = dbm.get_last_analysed_frames(
 #     "223344",
 #     "2019-12-02")
-# result_dsd = dbm.get_dash_stream_devices(
-#     ["223344", "uservju0ng0sl"],
-#     "2019-12-02")
-result_dt = dbm.get_dash_total(
-    "2019-12-03")
+result_dsd = dbm.get_dash_stream_devices(
+    "223344+111222",
+    "2019-12-05")
+# result_dt = dbm.get_dash_total(
+#     "2019-12-03")
 
-print(result_dt)
+print(result_dsd)

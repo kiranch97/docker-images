9fbc2ff39ec4557998d22a8d7a9f806b7f921d2d
GPU dev server <stanguldemond@gmail.com>
2020-02-10 17:21:50 +0100

    detection + saving file working in stack

--- odk-frame-analyzer/odkframelib/DiskWriter.py
+++ odk-frame-analyzer/odkframelib/DiskWriter.py
@@ -7,18 +7,26 @@ class DiskWriter:
     def __init__(self):
         pass
 
-    async def save_file(self, analysed_frame_data: dict, folder_path):
+    def save_file(self, analysed_frame_data: dict, something_detected):
 
-        app_id = analysed_frame_data["app_id"]
+        if something_detected:
+            folder_path = './data/something_detected'
+        else:
+            folder_path = './data/nothing_detected'
+
+        # Setup all the needed variables
+        app_id = analysed_frame_data['take_frame']['app_id']
         img = analysed_frame_data['take_frame']['img']
+        timestamp = analysed_frame_data['take_frame']['timestamp']
         gps_lat = analysed_frame_data['location']['lat']
         gps_lng = analysed_frame_data['location']['lng']
-        timestamp = analysed_frame_data['timestamp']
-
         frame_date = datetime.now().strftime("%Y-%m-%d")
 
+        # Decode base64 image
         imgdata = base64.b64decode(img.replace("data:image/jpeg;base64,", ""))
-        gps_location = "{0}, {1}".format(gps_lat, gps_lng)
+
+        # Set file name and location
+        gps_location = "{0}_{1}".format(gps_lat, gps_lng)
         filepath = "{0}/{1}/{2}".format(folder_path,
                                         frame_date,
                                         app_id)
@@ -30,71 +38,10 @@ class DiskWriter:
         filename = "{0}_{1}.jpg".format(timestamp, gps_location)
         full_filename = "{0}/{1}".format(filepath, filename)
 
+        # Save file to filesystem
         with open(full_filename, 'wb') as f:
             f.write(imgdata)
-
             print("File saved called {}".format(full_filename))
 
-        # Needed extras
-        # frame_date = datetime.now().strftime("%Y-%m-%d")
-
-        # if 'frame_name' in analysed_frame_data.keys():
-        #     # Frame with objects
-        #     FRAME_DIR = "./data/odk/images/objects/"
-
-        #     img = analysed_frame_data.get(
-        #         'take_frame', {}).get(
-        #             'img')
-        #     app_id = analysed_frame_data.get(
-        #         'take_frame', {}).get(
-        #             'app_id')
-        #     gps_lat = analysed_frame_data.get(
-        #         'take_frame', {}).get(
-        #             'lat')
-        #     gps_lng = analysed_frame_data.get(
-        #         'take_frame', {}).get(
-        #             'lng')
-        #     timestamp = analysed_frame_data.get(
-        #         'take_frame', {}).get(
-        #             'timestamp').replace(
-        #                 ':', '-')
-        # else:
-        #     # Frame without objects
-        #     FRAME_DIR = "./data/odk/images/no-objects/"
-
-        #     img = analysed_frame_data.get(
-        #         'img')
-        #     app_id = analysed_frame_data.get(
-        #         'app_id')
-        #     gps_lat = analysed_frame_data.get(
-        #         'lat')
-        #     gps_lng = analysed_frame_data.get(
-        #         'lng')
-        #     timestamp = analysed_frame_data.get(
-        #         'timestamp').replace(
-        #             ':', '-')
-
-        # if app_id is None:
-        #     # print("No Device data, file not saved")
-        #     pass
-        # elif gps_lat is None or gps_lng is None:
-        #     # print("No GPS data, file not saved")
-        #     pass
-        # else:
-        #     imgdata = base64.b64decode(img.replace("data:image/jpeg;base64,", ""))
-        #     gps_location = "{0}, {1}".format(gps_lat, gps_lng)
-        #     filepath = "{0}/{1}/{2}".format(FRAME_DIR,
-        #                                     frame_date,
-        #                                     app_id)
-
-        #     if not os.path.exists(filepath):
-        #         os.makedirs(filepath)
-        #         # print("made new directory called {}".format(filepath))
-
-        #     filename = "{0} {1}.jpg".format(timestamp, gps_location)
-        #     full_filename = "{0}/{1}".format(filepath, filename)
-
-        #     with open(full_filename, 'wb') as f:
-        #         f.write(imgdata)
-
-        #         print("file saved called {}".format(full_filename))
+        # Return the file location to store this in database
+        return full_filename

249525af8bcdb5f6b1e86308b348499848e6afaf
YoussefKassem <usfkassem@gmail.com>
2020-02-26 16:29:56 +0100

    Merging appRefactoring branch with master

--- odk-app/src/routes.js
+++ odk-app/src/routes.js
@@ -1,6 +1,5 @@
-// import Onboarding from './components/Onboarding.vue';
-import StreamingClient2 from './components/StreamingClient2';
-// import DeviceLogin from './components/DeviceLogin.vue';
+
+import StreamingClient from './components/StreamingClient';
 import BrowserStartPage from './components/BrowserStartPage.vue';
 import PwaStartPage from './components/PwaStartPage.vue';
 import RecommendationPage from './components/RecommendationPage.vue';
@@ -8,42 +7,12 @@ import ChecklistPage from './components/ChecklistPage.vue';
 
 
 export const routes = [
-    // {
-    //     path: '/index.html',
-    //     name : 'onboarding',
-    //     component: Onboarding,
-    //     alias: '/'
-    // },
+
     {
         path: '/client',
         name : 'streaming-client',
-        component: StreamingClient2
+        component: StreamingClient
     },
-    // {
-    //     path: '/login',
-    //     name: 'login',
-    //     component: DeviceLogin
-    // },
-    {
-        path: '/',
-        name: 'browser-startpage',
-        component: BrowserStartPage
-    },
-    {
-        path: '/pwa',
-        name: 'pwa-startpage',
-        component: PwaStartPage
-    },
-    {
-        path: '/recommendation',
-        name: 'recommendation-page',
-        component: RecommendationPage
-    },
-    // {
-    //     path: '/login',
-    //     name: 'login',
-    //     component: DeviceLogin
-    // },
     {
         path: '/',
         name: 'browser-startpage',

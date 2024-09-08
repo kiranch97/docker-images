1f21e4ad894130ee729007d6679ae384971300e3
Kiran Kumar <38249412+kiranch97@users.noreply.github.com>
2024-09-08 18:26:21 +0400

    Create app.js

---
+++ sample-app/app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.end('Hello, World!');
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
